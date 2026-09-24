f62d0fde09e9fc51bcabef8f69fe77eba4a0cf49
Abdulkhaliq59 <kananuraabdulkhaliq59@gmail.com>
2026-09-24 10:56:12 +0200

    Initial Commit

---
+++ IbangaChat/IbangaChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  IbangaChatApp.swift
+//  IbangaChat
+//
+//  Created by khaliq on 24/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct IbangaChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
